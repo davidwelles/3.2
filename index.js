94a227d495cb6b130b1110e14fd5fc99cf4e0ce9
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-03-02 17:05:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// let superHeroes = [
+//     ["Batman", "Bruce Wayne"],
+//     ["Spiderman", "Peter Parker"],
+//     ["The Flash", "Barry Allen"]
+//   ]
+
+//   let secretIdentity = superHeroes.map(function(revealArray){
+//       return revealArray.join("is");
+//   }
+
+//   console.log (secretIdentity.join("\n"));
+
+// let food = ['burgers', 'salad', 'tomatoes', 'fries'];
+// let newFood= food.join(" ");
+// console.log(newFood);
+
+
+// / let math = function (num) { return num + 5 };
+
+// // let arr = [5, 7, 8];
+
+// // arr.foreach(number => math(number));
+
+
+// let myFunction = function (element, index) {
+//   console.log(index + ":" + element);
+// }
+// //array
+// var fruits = ["apple", "orange", "cherry"];
+
+// // loop through
+// fruits.forEach(myFunction = (element, index) => {
+//   console.log(index + ":" + element);
+// });
+
+//Example of forEach
+// let arr=[1,2,3,4,5,6]
+
+
+// let newarr=[]
+// arr.forEach((item,index) => { newarr.push(item+index); })
+// console.log(newarr)
+
+//Example of Filter
+// let evenArr=[]
+// evenArr=arr.filter(item=> item%2==0)
+// console.log("even numbers\n"+evenArr)
+
+//Example of map
+// let powarr=[]
+// powarr=arr.map(function(item){return Math.pow(item,3)})
+// console.log("to the power of 3 \n"+powarr)
+
+//Example of reduce
+// let sum=arr.reduce((total,item)=>total+item)
+// console.log("The sum of the array items \n"+sum)
+
+// var dollars = [29.76, 41.85, 46.5];
+// // accumulator
+// var sum = dollars.reduce((acc, curr) => {
+//   acc = acc + curr;
+//   return acc;
+// }, 0);
+
+// // var sum = dollars.reduce(function (total, amount) {
+// //   console.log(total);
+// //   return total + amount;
+// // });
+
+// console.log(sum) // 118.11
+
